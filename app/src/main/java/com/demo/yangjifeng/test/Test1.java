95c36fe9a26b4a133e19242b3ee298aedcc57eb8
yangjifeng <axi.yangjifeng@aliyun.com>
2018-06-12 22:17:57 +0800

    新增测试Test1

---
+++ app/src/main/java/com/demo/yangjifeng/test/Test1.java
@@ -0,0 +1,16 @@
+package com.demo.yangjifeng.test;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Test1 {
+    private Context context;
+
+    public Test1(Context context) {
+        this.context = context;
+    }
+
+    public void addGit(String string) {
+        Toast.makeText(context, "哈哈", Toast.LENGTH_SHORT).show();
+    }
+}
